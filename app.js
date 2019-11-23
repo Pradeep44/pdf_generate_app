098e11f7c6349a07f9e3fa4a622abd157923878d
Pradeep44 <pradeepmudvari066@gmail.com>
2019-11-23 20:28:36 +0545

    minor fix

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const mongoose = require('mongoose')
 const pdfGenerateRequestController = require('./controller/pdfGenerator')
 const dailyActivityRouter = require('./controller/dailyActivityReportGenerator')
 
-
 mongoose.connect(process.env.mongoUri, {
   user:process.env.mongoUser,
   pass:process.env.mongoPassword
